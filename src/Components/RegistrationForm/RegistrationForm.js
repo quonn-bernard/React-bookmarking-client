cd061351085ac2a9b45ef0e62b79e1c10c3e8fd0
quonn <qber83@gmail.com>
2019-07-22 00:37:44 -0700

    adds LoginForm and RegistrationForm

---
+++ src/Components/RegistrationForm/RegistrationForm.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+import AuthApiService from '../../services/auth-api-service'
+
+
+export default class RegistrationForm extends Component {
+  static defaultProps = {
+    onRegistrationSuccess: () => {}
+  }
+
+  state = { error: null }
+
+  handleSubmit = ev => {
+    ev.preventDefault()
+    const { username, password } = ev.target
+    
+    this.setState({ error: null })
+    AuthApiService.postUser({
+      username: username.value,
+      password: password.value,
+    })
+      .then(user => {
+        username.value = ''
+        password.value = ''
+        this.props.onRegistrationSuccess(user)
+      })
+      .catch(res => {
+        this.setState({ error: res.error })
+      })
+  }
+
+  render() {
+    const { error } = this.state
+    return (
+      <form
+        className='RegistrationForm'
+        onSubmit={this.handleSubmit}
+      >
+        <div role='alert'>
+          {error && <p className='red'>{error}</p>}
+        </div>
+        <div className='username'>
+          <label htmlFor='RegistrationForm__username'>
+            User name
+          </label>
+          <input
+            name='username'
+            type='text'
+            required
+            id='RegistrationForm__username'>
+          </input>
+        </div>
+        <div className='password'>
+          <label htmlFor='RegistrationForm__password'>
+            Password
+          </label>
+          <input
+            name='password'
+            type='password'
+            required
+            id='RegistrationForm__password'>
+          </input>
+        </div>
+        <button type='submit'>
+          Register
+        </button>
+      </form>
+    )
+  }
+}
