e160d2b6a6e1e4e66786dd1f6cb97475bd6b429e
quonn <qber83@gmail.com>
2019-08-14 12:45:50 -0700

    removed logout functionality and added icon

--- src/Components/AccountPanel/AccountPanel.js
+++ src/Components/AccountPanel/AccountPanel.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import ProfileApiService from '../../services/profile-api-service';
-import { Link } from 'react-router-dom';
-import TokenService from '../../services/token-service';
+import AsideBtn from '../../Components/AsideBtn/AsideBtn';
 
 export default class AccountPanel extends React.Component {
-    logout(){
-        TokenService.clearAuthToken()
-        this.setState({
-            profile:{}
-        })
-    }
+
 
     render() {
         return (
-            <div>{this.props.profile.username}
-            {(TokenService.hasAuthToken())
-                ?  < Link onClick={this.logout}> <h4>Logout</h4> </Link>
-                :'Please Login'}
-                </div>
+            <AsideBtn icon={this.props.icon} name={this.props.profile.username}></AsideBtn>
         )
     }
 }
