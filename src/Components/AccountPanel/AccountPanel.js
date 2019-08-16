1292f576e7b9efbeabe36b276274b762ded083d5
quonn <qber83@gmail.com>
2019-08-16 11:12:49 -0700

    added comments

--- src/Components/AccountPanel/AccountPanel.js
+++ src/Components/AccountPanel/AccountPanel.js
@@ -3,9 +3,9 @@ import AsideBtn from '../../Components/AsideBtn/AsideBtn';
 
 export default class AccountPanel extends React.Component {
 
-
     render() {
         return (
+            // returns a sidebar btn with icon and username text
             <AsideBtn icon={this.props.icon} name={this.props.profile.username}></AsideBtn>
         )
     }
