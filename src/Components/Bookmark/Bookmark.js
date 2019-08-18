4f1a084f86c63caa92af1134f09920c48dc51729
quonn <qber83@gmail.com>
2019-08-18 14:15:24 -0700

    removed commented out/uneeded code. Added logic for hover dependent buttons

--- src/Components/Bookmark/Bookmark.js
+++ src/Components/Bookmark/Bookmark.js
@@ -4,14 +4,30 @@ import appContext from "../../appContext/appContext"
 import config from "../../config";
 import PropTypes from 'prop-types';
 import TokenService from "../../services/token-service";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt, faPencilAlt } from '@fortawesome/free-solid-svg-icons';
 
 class Bookmark extends Component {
+  // constructor(props) {
+  //   super(props);
 
+  //   this.state = {
+  //     hover: false
+  //   }
+  // }
   static defaultProps = {
     onDelete: () => { },
+    hover: false
   }
 
+  state={
+    hover: false
+  }
+
+
+
   static contextType = appContext;
+
 
   handleClickDelete = e => {
     e.preventDefault()
@@ -39,10 +55,45 @@ class Bookmark extends Component {
       })
   }
 
+    hoverOn = () => {
+    this.setState({ hover: true });
+    }
+
+    hoverOff = () => {
+    this.setState({ hover: false });
+  }
+  //   ,
+  //   hoverOn: function(){
+  //   this.setState({ hover: true });
+  // },
+  // hoverOff: function() {
+  //   this.setState({ hover: false });
+  // },
+  // render: function() {
+  //   return (
+  //     <i
+  //       className={this.state.hover ? "fa fa-heart" : "fa fa-heart-o"}
+  //       onMouseEnter={this.hoverOn}
+  //       onMouseLeave={this.hoverOff}
+  //     >
+  //       {" "} My class is: {this.state.hover ? "fa-heart" : "fa-heart-o"}
+  //     </i>
+  //   );
+  // }
+
+
+
+
   render() {
+    const trashIcon = <FontAwesomeIcon style={{ fontSize: "12px", color: "springgreen"}} icon={faTrashAlt} />
+    const penIcon = <FontAwesomeIcon style={{ fontSize: "12px", color: "springgreen"}} icon={faPencilAlt} />
     const { name, id, type } = this.props
+    let btnClass;
+    (this.state.hover)
+    ? btnClass = ""
+    : btnClass = "hidden"
     return (
-      <>
+      <div onMouseEnter={this.hoverOn} onMouseLeave={this.hoverOff}>
         <div key={id} className="bookmark-top">
           <header>
             <p>({type})</p>
@@ -51,14 +102,16 @@ class Bookmark extends Component {
           <h1>
             <Link to={'/bookmark/' + id}> "{name}"  </Link>
           </h1>
-          <p>CREATED BY: <span style={{color: "springgreen", textTransform: "uppercase"}}>{this.context.profile.username}</span></p>
+          <p>CREATED BY: <span style={{ color: "springgreen", textTransform: "uppercase" }}>{this.context.profile.username}</span></p>
 
 
         </div>
         <div className="bookmark-bottom">
-          <button type="button" onClick={this.handleClickDelete}>Delete</button>
+          <button type="button" className={btnClass} onClick={this.handleClickDelete}>{trashIcon} Delete</button>
+          <span></span>
+          <button type="button" className={btnClass} >{penIcon} Edit</button>
         </div>
-      </>
+      </div>
     )
   }
 }
